df2678bbc4eac4c0ced892a4cb7a833760e2a38b
numanabid <numanabid27@gmail.com>
2025-09-06 02:54:55 +0500

    add fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,12 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/common/components/header/header";
 import Footer from "@/common/components/footer/footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
+  variable: "--font-poppins-sans",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], // ✅ add available weights
 });
 
 export const metadata = {
@@ -22,10 +18,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.variable} antialiased`}
       >
         <Header />
-        {children}
+          {children}
         <Footer />
       </body>
     </html>

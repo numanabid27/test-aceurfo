07f5ebcb9c5fc700529081776264059a7383a887
numanabid <numanabid27@gmail.com>
2025-10-06 03:14:47 +0500

    update blog

--- src/components/blogs/blogs.component.jsx
+++ src/components/blogs/blogs.component.jsx
@@ -1,31 +1,34 @@
 import React from "react";
 import { BLOGS } from "./blogs.constant";
 import Banner from "@/common/components/banner/banner";
+import Image from "next/image";
 
 export default function Blogs() {
 
 
   return (
     <>
-      <Banner title="Blogs" desc={<>At <span class="font-semibold">Bookkeeping.com</span>, we offer a wide
+      <Banner title="Blogs" desc={<>At <span className="font-semibold">Bookkeeping.com</span>, we offer a wide
           range of financial services tailored to meet the diverse needs of our
           clients. Our team of experts is committed to delivering exceptional
           service and strategic solutions. Explore our services below:</>} />
       <section className="py-20 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {BLOGS.map((blog) => (
             <div
               key={blog.id}
               className="bg-white rounded-2xl shadow-md hover:shadow-xl transition duration-300 overflow-hidden"
             >
-              <img
+              <Image
                 src={blog.image}
                 alt={blog.title}
                 className="w-full h-48 object-cover"
+                width={300}
+                height={300}
               />
               <div className="p-5">
                 <p className="text-sm text-gray-500">{blog.date}</p>
-                <h3 className="text-lg font-semibold text-gray-800 mt-2 line-clamp-2">
+                <h3 className="text-lg font-semibold text-gray-800 mt-2 line-clamp-2" title={blog.title}>
                   {blog.title}
                 </h3>
                 <p className="text-gray-600 text-sm mt-2 line-clamp-3">
